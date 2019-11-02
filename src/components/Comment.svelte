c447c61c2e409eb4490b44128fe87f1680d96804
Kevin <kevin.aberg@gmail.com>
2019-11-02 13:01:59 +0100

    feature: adds comment page functionality

---
+++ src/components/Comment.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let comment;
+</script>
+
+<style>
+  .container {
+    margin-left: 25px;
+    margin-top: 20px;
+  }
+  p {
+    color: grey;
+    font-size: 0.9em;
+  }
+  .html {
+    border-bottom: 1px solid lightgrey;
+  }
+</style>
+
+{#each comment.comments as comment}
+  <div class="container">
+    <p>{comment.user} {comment.time_ago}</p>
+    <div class="html">
+      {@html comment.content}
+    </div>
+    <svelte:self {comment} />
+  </div>
+{/each}
--- src/pages/SingleItem.svelte
+++ src/pages/SingleItem.svelte
@@ -1,5 +1,17 @@
 <script>
+  import Comment from "../components/Comment.svelte";
+
   export let id;
+
+  let promise = fetch(`http://node-hnapi.herokuapp.com/item/${id}`).then(res =>
+    res.json()
+  );
 </script>
 
-This is ID: {id}
+{#await promise}
+  <h2>Loading...</h2>
+{:then data}
+  <h2>{data.title}</h2>
+  <p>submitted by {data.user} {data.time_ago}</p>
+  <Comment comment={data} />
+{/await}
