f86d8665d3779feb0217c479dcb7c1a77a0308b5
Kevin <kevin.aberg@gmail.com>
2019-11-02 12:28:19 +0100

    adds basic structure for application.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
-	export let name;
+  import { Router, Route } from "svelte-routing";
+
+  import Navigation from "./components/Navigation.svelte";
+  import Home from "./pages/Home.svelte";
+  import SingleItem from "./pages/SingleItem.svelte";
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  div {
+    max-width: 700px;
+    margin: 0 auto;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<Router>
+  <Navigation />
+  <div>
+    <Route path="/item/:id" component={SingleItem} />
+    <Route path="/" component={Home} />
+  </div>
+</Router>
---
+++ src/components/Navigation.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Link } from "svelte-routing";
+</script>
+
+<style>
+  nav {
+    background: orange;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    padding: 10px 0;
+  }
+  ul {
+    height: 30px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    list-style: none;
+    margin: 0;
+    align-items: center;
+  }
+</style>
+
+<nav>
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+  </ul>
+</nav>
